8fda15acd6eb8d8afaa4de08ce13471fd19ce495
Martin Brennan <mbrenn3@uic.edu>
2014-06-05 16:17:39 -0500

    started average.m

---
+++ average.m
@@ -0,0 +1,10 @@
+load data/050714/analyzed1.txt
+load data/050914/analyzed2.txt
+load data/051214/analyzed3.txt
+
+well1 = analyzed1(:,1);
+well2 = analyzed1(:,2);
+well3 = analyzed1(:,3);
+well4 = analyzed1(:,4);
+well5 = analyzed1(:,5);
+well6 = analyzed1(:,6);
