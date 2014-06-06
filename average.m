4da60d50eb1b53435cdad428bd11ddbf47253cf3
Martin Brennan <mbrenn3@uic.edu>
2014-06-06 14:43:29 -0500

    added export line and comments to average.m

--- average.m
+++ average.m
@@ -1,9 +1,17 @@
+% This m-file calulates the average and standard deviation for 6 colums(wells) of data from 3 runs
+
+
+%loads data into matrices
 load data/050714/analyzed1.txt
 load data/050914/analyzed2.txt
 load data/051214/analyzed3.txt
 
+% calculation for average
 average = [(analyzed1(:,1)+analyzed2(:,1)+analyzed3(:,1))/3,(analyzed1(:,2)+analyzed2(:,2)+analyzed3(:,2))/3,(analyzed1(:,3)+analyzed2(:,3)+analyzed3(:,3))/3,(analyzed1(:,4)+analyzed2(:,4)+analyzed3(:,4))/3,(analyzed1(:,5)+analyzed2(:,5)+analyzed3(:,5))/3,(analyzed1(:,6)+analyzed2(:,6)+analyzed3(:,6))/3]; 
 
-save -ascii data/average.txt average
+save -ascii data/average.txt average % exports average to a file
 
-stdev =  [((analyzed1(:,1)-average(:,1)).**2 + (analyzed2(:,1)-average(:,1)).**2 + (analyzed3(:,1)-average(:,1)).**2)/3,((analyzed1(:,2)-average(:,2)).**2 + (analyzed2(:,2)-average(:,2)).**2 + (analyzed3(:,2)-average(:,2)).**2)/3,((analyzed1(:,3)-average(:,3)).**2 + (analyzed2(:,3)-average(:,3)).**2 + (analyzed3(:,3)-average(:,3)).**2)/3,((analyzed1(:,4)-average(:,4)).**2 + (analyzed2(:,4)-average(:,4)).**2 + (analyzed3(:,4)-average(:,4)).**2)/3,((analyzed1(:,5)-average(:,5)).**2 + (analyzed2(:,5)-average(:,5)).**2 + (analyzed3(:,5)-average(:,5)).**2)/3,((analyzed1(:,6)-average(:,6)).**2 + (analyzed2(:,6)-average(:,6)).**2 + (analyzed3(:,6)-average(:,6)).**2)/3]
+% calulation for standard deviation 
+stdev =  [((analyzed1(:,1)-average(:,1)).**2 + (analyzed2(:,1)-average(:,1)).**2 + (analyzed3(:,1)-average(:,1)).**2)/3,((analyzed1(:,2)-average(:,2)).**2 + (analyzed2(:,2)-average(:,2)).**2 + (analyzed3(:,2)-average(:,2)).**2)/3,((analyzed1(:,3)-average(:,3)).**2 + (analyzed2(:,3)-average(:,3)).**2 + (analyzed3(:,3)-average(:,3)).**2)/3,((analyzed1(:,4)-average(:,4)).**2 + (analyzed2(:,4)-average(:,4)).**2 + (analyzed3(:,4)-average(:,4)).**2)/3,((analyzed1(:,5)-average(:,5)).**2 + (analyzed2(:,5)-average(:,5)).**2 + (analyzed3(:,5)-average(:,5)).**2)/3,((analyzed1(:,6)-average(:,6)).**2 + (analyzed2(:,6)-average(:,6)).**2 + (analyzed3(:,6)-average(:,6)).**2)/3];
+
+save -ascii data/stdev.txt stdev  % exports stdev to a file
