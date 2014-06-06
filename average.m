c92f252d5b6d93a9831a834a3da5219f1d920b79
Martin Brennan <mbrenn3@uic.edu>
2014-06-06 13:43:19 -0500

    average.m working changed the names of analyzedx.txt

--- average.m
+++ average.m
@@ -2,9 +2,6 @@ load data/050714/analyzed1.txt
 load data/050914/analyzed2.txt
 load data/051214/analyzed3.txt
 
-well1 = analyzed1(:,1);
-well2 = analyzed1(:,2);
-well3 = analyzed1(:,3);
-well4 = analyzed1(:,4);
-well5 = analyzed1(:,5);
-well6 = analyzed1(:,6);
+average = [(analyzed1(:,1)+analyzed2(:,1)+analyzed3(:,1))/3,(analyzed1(:,2)+analyzed2(:,2)+analyzed3(:,2))/3,(analyzed1(:,3)+analyzed2(:,3)+analyzed3(:,3))/3,(analyzed1(:,4)+analyzed2(:,4)+analyzed3(:,4))/3,(analyzed1(:,5)+analyzed2(:,5)+analyzed3(:,5))/3,(analyzed1(:,6)+analyzed2(:,6)+analyzed3(:,6))/3]; 
+
+save -ascii data/average.txt average
