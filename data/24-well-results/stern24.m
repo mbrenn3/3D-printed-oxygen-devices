e59e36a1e902ffcbcc6f469dd602616b2a8b143f
mbrenn3 <mbrenn3@uic.edu>
2015-06-10 10:54:14 -0500

    working on adjusted tau

--- data/24-well-results/stern24.m
+++ data/24-well-results/stern24.m
@@ -8,7 +8,7 @@ amb_mean = mean(r(15:18,:));
 intensities = [ss_mean;zeros(1,columns(r));amb_mean;(ones(1,columns(r)))*21;r(11:rows(r),:)];
 
 number_of_wells = columns (intensities); % finds the number of columns in the matrix
-a = zeros (size (intensities));
+a = zeros (size (intensities)-4);
 b=0;
 i=1;
 for i = 1:columns (intensities)
@@ -19,38 +19,32 @@ for i = 1:columns (intensities)
    calibration_measurment1 = well(2);
    calibration_measurment2 = well(4);
 
-kt = (calibration_intensity2-calibration_intensity1)/((calibration_measurment1*calibration_intensity1)-(calibration_measurment2*calibration_intensity2)); % calculation for Ktau
+   kt = (calibration_intensity2-calibration_intensity1)/((calibration_measurment1*calibration_intensity1)-(calibration_measurment2*calibration_intensity2)); % calculation for Ktau
 
-I0 = (1+kt*calibration_measurment1)*calibration_intensity1; % intensity in absence of quencher(0% O2)
+   I0 = (1+kt*calibration_measurment1)*calibration_intensity1; % intensity in absence of quencher(0% O2)
 
-x1 = 0:1:100;              % makes x values for plot of stern-volmer
-y1 = I0./(1+kt*x1);        % plots stern-volmer. Check to see if its fitting your calibration points.
+   %x1 = 0:1:100;              % makes x values for plot of stern-volmer
+   %y1 = I0./(1+kt*x1);        % plots stern-volmer. Check to see if its fitting your calibration points.
 
 %plot(y1,x1); % added
+   int = well(5:rows(well),:);
+   calculated_percent = ((I0./int-1))/kt;
+   c1 = calculated_percent;
 
-calculated_percent = ((I0./well)-1)/kt;
+   for n = 1:1
 
-  for n=100
+     adjusted_tau = 0.0292*calculated_percent + 1.022 ; 
+     adjusted_percent = ((I0./int)-1)/(kt*adjusted_tau(i));
+     calculated_percent = adjusted_percent;
+     n = n+1;
 
-  
-  
-  
-  tau_for_real = .03*calculated_percent + 1 ; 
+   endfor
 
-  calculated_percent_new = ((I0./well)-1)/(kt*tau_for_real(i));
-
-  %a(:,i) = calculated_percent;
-  a2(:,i) = calculated_percent_new;
-  
-  n = n+1
+a(:,i) = adjusted_percent;
 
 endfor
-endfor
 
-eval = a(5:rows(a),:);
-eval2 = a2(5:rows(a2),:);
-
- save -ascii evaluated1.csv eval
- save -ascii evaluated2.csv eval2
+%eval = a(5:rows(a),:);
 
 
+save -ascii new.csv a
